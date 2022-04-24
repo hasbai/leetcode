a611cf887b1805b7b3ce27990a4a21fe3c44e8bf
顶碗人 <jsclndnz@outlook.com>
2022-04-24 13:14:20 +0800

    ContainerWithMostWater

---
+++ leetcode/editor/en/No11ContainerWithMostWater.java
@@ -0,0 +1,68 @@
+//You are given an integer array height of length n. There are n vertical lines 
+//drawn such that the two endpoints of the iᵗʰ line are (i, 0) and (i, height[i]).
+// 
+//
+// Find two lines that together with the x-axis form a container, such that the 
+//container contains the most water. 
+//
+// Return the maximum amount of water a container can store. 
+//
+// Notice that you may not slant the container. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,
+//3,7]. In this case, the max area of water (blue section) the container can 
+//contain is 49.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: height = [1,1]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == height.length 
+// 2 <= n <= 10⁵ 
+// 0 <= height[i] <= 10⁴ 
+// 
+// Related Topics Array Two Pointers Greedy 👍 16564 👎 934
+
+
+package leetcode.editor.en;
+
+public class No11ContainerWithMostWater {
+    public static void main(String[] args) {
+        Solution solution = new No11ContainerWithMostWater().new Solution();
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int max = right * Math.min(height[left], height[right]);
+            while (left < right) {
+                if (height[left] <= height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+                max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+            }
+            return max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
